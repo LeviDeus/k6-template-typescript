7d1cd22da0f59eff0013546077785483857bd9ae
Levi Deuster <levi@metalface.com>
2023-05-25 11:45:21 +0200

    :3

--- src/getdatapoints-test.js
+++ src/getdatapoints-test.js
@@ -3,7 +3,16 @@ import { sleep } from 'k6';
 import { check } from 'k6';
 
 export const options = {
-    
+    stages: [
+        { duration: "1m", target: 100 }, //low traffic
+        { duration: "1m", target: 200 }, //ramp up to average traffic
+        { duration: "5m", target: 300 }, //slowly ramp up to high traffic
+        { duration: "1m", target: 0 }, //ramp down to 0 logins 
+    ],
+
+    thresholds: {
+        http_req_duration: [{ threshold: 'p(90) <500', abortOnFail: false }],
+    },
 }
 
 export default function () {
@@ -19,8 +28,8 @@ export default function () {
     }
     const res = http.get(url, payload, params);
     check(res, {
-        'has status 200': (r) => r.status === 200,
-        'response body has metals': (r) => r.body.includes('LME-CU', 'LME-SN', 'LME-CO'),
+        'has status 200': (res) => res.status === 200,
+        'response body has metals': (res) => res.body.includes('LME-CU', 'LME-SN', 'LME-CO'),
     })
     console.log(res.timings.duration + 'ms' + "VU" + __VU);
 };
